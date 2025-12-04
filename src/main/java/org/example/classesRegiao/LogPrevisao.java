b0b1085813d89800980421864124f1c9a1c87a83
Dereck-Baksa <dereck.baksa@sptech.school>
2025-12-03 23:18:25 -0300

    metodos regiao

--- src/main/java/org/example/classesRegiao/LogPrevisao.java
+++ src/main/java/org/example/classesRegiao/LogPrevisao.java
@@ -1,16 +1,20 @@
 package org.example.classesRegiao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.LocalDate;
 
 public class LogPrevisao {
+    private static final Logger log = LoggerFactory.getLogger(LogPrevisao.class);
     private LocalDate data;
     private Integer qtdRequisicao;
     private Double chanceDeChuva;
     private Double chuvaEmMM;
-    private Integer temperatura;
-    private Integer umidade;
+    private Double temperatura;
+    private Double umidade;
 
-    public LogPrevisao(Double chanceDeChuva, Double chuvaEmMM, LocalDate data, Integer qtdRequisicao, Integer temperatura, Integer umidade) {
+    public LogPrevisao(Double chanceDeChuva, Double chuvaEmMM, LocalDate data, Integer qtdRequisicao, Double temperatura, Double umidade) {
         this.chanceDeChuva = chanceDeChuva;
         this.chuvaEmMM = chuvaEmMM;
         this.data = data;
@@ -51,20 +55,80 @@ public class LogPrevisao {
         this.qtdRequisicao = qtdRequisicao;
     }
 
-    public Integer getTemperatura() {
+    public Double getTemperatura() {
         return temperatura;
     }
 
-    public void setTemperatura(Integer temperatura) {
+    public void setTemperatura(Double temperatura) {
         this.temperatura = temperatura;
     }
 
-    public Integer getUmidade() {
+    public Double getUmidade() {
         return umidade;
     }
 
-    public void setUmidade(Integer umidade) {
+    public void setUmidade(Double umidade) {
         this.umidade = umidade;
+    }
+
+    public Double qtdReqPrevistas(){
+        Double qtdReqPrevistas = 0.0;
+        qtdReqPrevistas += qtdRequisicao;
+
+        if (chanceDeChuva > 0) {
+            if (temperatura > 30 || temperatura < 0) {
+                qtdReqPrevistas += (qtdRequisicao * 0.05);
+            }
+
+            if (chuvaEmMM > 3.0) {
+                qtdReqPrevistas += qtdRequisicao * 0.35;
+            } else if (chuvaEmMM > 2.0) {
+                qtdReqPrevistas += qtdRequisicao * 0.25;
+            } else if (chuvaEmMM > 1.0) {
+                qtdReqPrevistas += qtdRequisicao * 0.10;
+            }else if (chuvaEmMM < 1.0) {
+                qtdReqPrevistas += qtdRequisicao * 0.05;
+            }
+        }
+
+
+        return qtdReqPrevistas;
+    }
+
+    public Double chanceDeAlteracao(){
+        Double chance = chanceDeChuva;
+
+        if (temperatura > 30 || temperatura < 0 ){
+            chance += chance * 0.005;
+        } else if (temperatura > 20 || temperatura < 10) {
+            chance += chance * 0.007;
+        }
+
+        if (chance > 100.00){
+            chance = 100.00;
+        }
+        return chance;
+    }
+
+    public Double usoRAM(Double req){
+        return req * 0.003;
+    }
+
+    public Double percentualDeAumento(Integer req,Integer reqPrevista){
+        Integer resultado;
+        if (req > reqPrevista){
+            resultado = req - reqPrevista ;
+        }else {
+            resultado = reqPrevista - req;
+        }
+
+
+        return (resultado/req.doubleValue()) * 100.00;
+
+
+
+
+
     }
 
     @Override
